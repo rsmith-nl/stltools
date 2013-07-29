a7a31dec230c7352742ae1a778721452ca34a0a1
Roland Smith <rsmith@xs4all.nl>
2013-07-29 23:04:56 +0200

    Rewrite vecops with numpy.

--- brep/vecops.py
+++ brep/vecops.py
@@ -25,82 +25,51 @@
 
 """Operations of two or three dimensional vectors."""
 
-def add(a, b):
-    return tuple(i+j for i, j in zip(a, b))
-
-
-def sub(a, b):
-    return tuple(i-j for i, j in zip(a, b))
-
-
-def mul(v, s):
-    return tuple(s*j for j in v)
-
-
-def div(v, s):
-    return tuple(j/s for j in v)
-
-
-def eq(a, b):
-    return all(i==j for i, j in zip(a, b))
-
-
-def ne(a, b):
-    return any(i!=j for i, j in zip(a, b))
+import numpy as np
+import math as m
 
 
 def length(v):
-    return sum(j*j for j in v)**0.5
+    """Returns the length of a vector.
+
+    :v: numpy vector
+    """
+    return m.sqrt(np.sum(v*v))
 
 
 def normalize(v):
+    """Returns the vector scaled to lenth 1.
+
+    :v: numpy vector
+    """
     l = length(v)
-    return div(v, l)
-
-
-def cross(a, b):
-    if len(a) == 3 and len(b) == 3:
-        ax, ay, az = a
-        bx, by, bz = b
-        return (ay * bz - az * by, az * bx - ax * bz, ax * by - ay * bx)
-    else:
-        raise ValueError('cross product only defined for 3D vectors')
-
-
-def dot(a, b):
-    return sum(i*j for i, j in zip(a, b))
-
-
-def mkstr(v, sep=', '):
-    return sep.join(str(j) for j in v)
+    return v/l
 
 
 def normal(a, b, c):
     """Calculate the normal vector for the triangle defined by a, b and c.
 
-    :a: 3-tuple of numbers
-    :b: 3-tuple of numbers
-    :c: 3-tuple of numbers
+    :a, b, c: numpy array of shape (3,)
     :returns: a vector normal to the plane formed by a, b and c.
     """
-    u = sub(b, a)
-    v = sub(c, b)
-    n = cross(u, v)
+    u = b - a
+    v = c - b
+    n = np.cross(u, v)
     l = length(n)
     if l:
-        return div(n, l)
+        return n/l
     return n
 
 
 def indexate(points):
-    """Convert a list of points into a list of indices and a list of unique
-    points.
+    """Convert a numpy array of points into a list of indices and an array of 
+    unique points.
 
-    :points: A list of points, where a point is a 3-tuple of numbers.
-    :returns: A tuple (indices, unique_points)
+    :points: a numpy array of shape (N, 3)
+    :returns: a tuple of indices and an array of unique points
     """
     pd = {}
-    indices = [pd.setdefault(p, len(pd)) for p in points]
+    indices = tuple(pd.setdefault(tuple(p), len(pd)) for p in points)
     pt = sorted([(v, k) for k, v in pd.iteritems()], key=lambda x: x[0])
-    unique = [i[1] for i in pt]
+    unique = np.array([i[1] for i in pt])
     return indices, unique
