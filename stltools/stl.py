514cfa96c45163f47ec820a1fdd902a1b858242a
Roland Smith <rsmith@xs4all.nl>
2020-10-04 14:53:02 +0200

    Converted stl.py and passed test suite.

--- stltools/stl.py
+++ stltools/stl.py
@@ -4,14 +4,15 @@
 # Copyright © 2012-2020 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
 # SPDX-License-Identifier: BSD-2-Clause
 # Created: 2012-11-10 07:55:54 +0100
-# Last modified: 2020-10-04T12:16:03+0200
+# Last modified: 2020-10-04T14:52:35+0200
 """Handling STL files and brep datasets."""
 
-import struct
-import mmap
 from . import vecops as vo
-import numpy as np
 import datetime
+import functools as ft
+import itertools as it
+import mmap
+import struct
 
 
 def readstl(name, encoding='utf-8'):
@@ -39,153 +40,6 @@ def readstl(name, encoding='utf-8'):
     if vertices is None:
         raise ValueError('not a valid STL file.')
     return vertices, name
-
-
-def toindexed(vertices):
-    """
-    Convert vertices to index format.
-
-    Create an array of unique vertices and an array of indices into the unique
-    vertax array that matches the original array.
-
-    Arguments:
-        vertices: (?, 3) array of vertex coordinates.
-
-    Returns:
-        An (?, 3) array of facet indices and an (M, 3) array of unique points.
-    """
-    ix, points = vo.indexate(vertices)
-    facets = ix.reshape((-1, 3))
-    return facets, points
-
-
-def normals(facets, points):
-    """
-    Calculate normal vectors of facets.
-
-    Arguments:
-        facets: An (?, 3) array of facet indices into points.
-        points: An (?, 3) array of unique points.
-
-    Returns:
-        an array of normal vector indices for each facet and an array of
-        unique normals.
-    """
-    nv = [vo.normal(points[i], points[j], points[k]) for i, j, k in facets]
-    return vo.indexate(nv)
-
-
-def text(name, ifacets, points, inormals, vectors):
-    """
-    Make an STL text representation of a brep.
-
-    Arguments:
-        name: The name of the object.
-        ifacets: A (?, 3) array of indices into the points.
-        points: An (?, 3) array of vertex coordinates.
-        inormals: An array of indices into the vectors list.
-        vectors: An (?, 3) array of normal vectors.
-
-    Returns:
-        A string containing a text representation of the brep.
-    """
-    fcts = list(zip(ifacets, inormals))
-    ln = ['solid {}'.format(name)]
-    for f, n in fcts:
-        ln.append('  facet normal ' + str(vectors[n])[1:-1])
-        ln.append('    outer loop')
-        for v in f:
-            ln.append('      vertex ' + str(points[v])[1:-1])
-        ln.append('    endloop')
-        ln.append('  endfacet')
-    ln.append('endsolid')
-    return '\n'.join(ln)
-
-
-def binary(name, ifacets, points, inormals, vectors):
-    """
-    Make an STL binary representation of a brep.
-
-    Arguments:
-        name: The name of the object.
-        ifacets: A (?, 3) array of indices into the points.
-        points: An (?, 3) array of vertex coordinates.
-        inormals: An array of indices into the vectors list.
-        vectors: An (?, 3) array of normal vectors.
-
-    Returns:
-        A string containing a binary representation of the brep.
-    """
-    rc = [struct.pack('<80sI', name.encode('utf-8'), len(ifacets))]
-    for fi, ni in zip(ifacets, inormals):
-        data = list(np.concatenate((vectors[ni], points[fi[0]], points[fi[1]], points[fi[2]]))
-                    ) + [0]
-        rc.append(struct.pack('<12fH', *data))
-    return b''.join(rc)
-
-
-def _striplines(m):
-    """
-    Generate stripped lines from a memmapped text file.
-
-    Arguments:
-        m: A memory mapped file.
-
-    Yields:
-        The stripped lines of the file as text.
-    """
-    while True:
-        v = m.readline().decode('utf-8')
-        if v:
-            yield v.strip()
-        else:
-            break
-
-
-def _parsetxt(m, encoding):
-    """
-    Parse a text STL file.
-
-    Arguments:
-        m: A memory mapped file.
-        encoding: For the name of the object.
-
-    Returns:
-        The vertices as a (?, 3) numpy array, and the name of the object from
-        the file.
-    """
-    first = m.readline().decode(encoding)
-    name = None
-    points = None
-    if (first.startswith('solid') and b'facet normal' in m.readline()):
-        try:
-            name = first.strip().split(None, 1)[1]
-        except IndexError:
-            name = ''
-        vlines = [ln.split() for ln in _striplines(m) if ln.startswith('vertex')]
-        points = np.array([tuple(float(k) for k in j[1:]) for j in vlines], np.float32)
-    m.seek(0)
-    return points, name
-
-
-def _getbp(m):
-    """
-    Generate points from a binary STL file.
-
-    Arguments:
-        m: A memory mapped file.
-
-    Yields:
-        The vertices as 3-tuple of floats.
-    """
-    while True:
-        v = m.read(50)
-        if len(v) != 50:
-            break
-        p = struct.unpack('<12x9f2x', v)
-        yield tuple(p[0:3])
-        yield tuple(p[3:6])
-        yield tuple(p[6:])
 
 
 def _parsebinary(m, encoding):
@@ -221,7 +75,167 @@ def _parsebinary(m, encoding):
     name = name.replace("solid ", "")
     name = name.strip('\x00 \t\n\r')
     points = [p for p in _getbp(m)]
-    return np.array(points, np.float32), name
+    return points, name
+
+
+def _getbp(m):
+    """
+    Generate points from a binary STL file.
+
+    Arguments:
+        m: A memory mapped file.
+
+    Yields:
+        The vertices as 3-tuple of floats.
+    """
+    while True:
+        v = m.read(50)
+        if len(v) != 50:
+            break
+        p = struct.unpack('<12x9f2x', v)
+        yield tuple(p[0:3])
+        yield tuple(p[3:6])
+        yield tuple(p[6:])
+
+
+def _parsetxt(m, encoding):
+    """
+    Parse a text STL file.
+
+    Arguments:
+        m: A memory mapped file.
+        encoding: For the name of the object.
+
+    Returns:
+        The vertices as a (?, 3) numpy array, and the name of the object from
+        the file.
+    """
+    first = m.readline().decode(encoding)
+    name = None
+    points = None
+    if (first.startswith('solid') and b'facet normal' in m.readline()):
+        try:
+            name = first.strip().split(None, 1)[1]
+        except IndexError:
+            name = ''
+        vlines = [ln.split() for ln in _striplines(m) if ln.startswith('vertex')]
+        points = [tuple(float(k) for k in j[1:]) for j in vlines]
+    m.seek(0)
+    return points, name
+
+
+def _striplines(m):
+    """
+    Generate stripped lines from a memmapped text file.
+
+    Arguments:
+        m: A memory mapped file.
+
+    Yields:
+        The stripped lines of the file as text.
+    """
+    while True:
+        v = m.readline().decode('utf-8')
+        if v:
+            yield v.strip()
+        else:
+            break
+
+
+def toindexed(vertices):
+    """
+    Convert vertices to index format.
+
+    Create an array of unique vertices and an array of indices into the unique
+    vertax array that matches the original array.
+
+    Arguments:
+        vertices: (?, 3) array of vertex coordinates.
+
+    Returns:
+        An (?, 3) array of facet indices and an (M, 3) array of unique points.
+    """
+    ix, points = vo.indexate(vertices)
+    facets = list(_chunked(ix, 3))
+    return facets, points
+
+
+def _chunked(iterable, n):
+    """
+    Split an iterable up in chunks of length n.
+
+    The second argument to the outer ``iter()`` is crucial to the way this works.
+    See the documentation for ``iter()`` for details.
+    """
+    def take(n, iterable):
+        return list(it.islice(iterable, n))
+    return iter(ft.partial(take, n, iter(iterable)), [])
+
+
+def normals(facets, points):
+    """
+    Calculate normal vectors of facets.
+
+    Arguments:
+        facets: An (?, 3) array of facet indices into points.
+        points: An (?, 3) array of unique points.
+
+    Returns:
+        an array of normal vector indices for each facet and an array of
+        unique normals.
+    """
+    nv = [vo.normal(points[i], points[j], points[k]) for i, j, k in facets]
+    return vo.indexate(nv)
+
+
+def text(name, ifacets, points, inormals, vectors):
+    """
+    Make an STL text representation of a brep.
+
+    Arguments:
+        name: The name of the object.
+        ifacets: A (?, 3) array of indices into the points.
+        points: An (?, 3) array of vertex coordinates.
+        inormals: An array of indices into the vectors list.
+        vectors: An (?, 3) array of normal vectors.
+
+    Returns:
+        A string containing a text representation of the brep.
+    """
+    fcts = list(zip(ifacets, inormals))
+    ln = ['solid {}'.format(name)]
+    for f, n in fcts:
+        ln.append(f'  facet normal {vectors[n][0]:.1f} {vectors[n][1]:.1f} {vectors[n][2]:.1f}')
+        ln.append('    outer loop')
+        for v in f:
+            ln.append(f'      vertex {points[v][0]:.1f} {points[v][1]:.1f} {points[v][2]:.1f}')
+        ln.append('    endloop')
+        ln.append('  endfacet')
+    ln.append('endsolid')
+    return '\n'.join(ln)
+
+
+def binary(name, ifacets, points, inormals, vectors):
+    """
+    Make an STL binary representation of a brep.
+
+    Arguments:
+        name: The name of the object.
+        ifacets: A (?, 3) array of indices into the points.
+        points: An (?, 3) array of vertex coordinates.
+        inormals: An array of indices into the vectors list.
+        vectors: An (?, 3) array of normal vectors.
+
+    Returns:
+        A string containing a binary representation of the brep.
+    """
+    rc = [struct.pack('<80sI', name.encode('utf-8'), len(ifacets))]
+    for fi, ni in zip(ifacets, inormals):
+        # data = list(np.concatenate((vectors[ni], points[fi[0]], points[fi[1]], points[fi[2]]))
+        #             ) + [0]
+        data = list(vectors[ni] + points[fi[0]] + points[fi[1]] + points[fi[2]]) + [0]
+        rc.append(struct.pack('<12fH', *data))
+    return b''.join(rc)
 
 
 def _test(args):
--- test/test_stl.py
+++ test/test_stl.py
@@ -3,40 +3,39 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2015-08-22 16:45:36 +0200
-# Last modified: 2019-08-27T20:42:29+0200
+# Last modified: 2020-10-04T14:31:00+0200
 """
 Tests for the stl module.
 
-Run this test only with py.test-3.5 -v test_stl.py
-Run all tests with: py.test-3.5 -v test_*
+Run this test only with py.test-v test_stl.py
+Run all tests with: py.test -v test_*
 """
 
-import numpy as np
 import stltools.stl as stl
 
 
 def test_read_bin():
     vertices, name = stl.readstl('test/data/cube-bin.stl')
     assert name == 'cube_bin'
-    assert vertices.shape == (36, 3)
+    assert len(vertices) == 36 and len(vertices[0]) == 3
     _, pnts = stl.toindexed(vertices)
-    assert pnts.shape == (8, 3)
+    assert len(pnts) == 8 and len(pnts[0]) == 3
 
 
 def test_read_txt():
     vertices, name = stl.readstl('test/data/cube-txt.stl')
     assert name == 'cube_txt'
-    assert vertices.shape == (36, 3)
+    assert len(vertices) == 36 and len(vertices[0]) == 3
     _, pnts = stl.toindexed(vertices)
-    assert pnts.shape == (8, 3)
-    assert np.all(pnts[0] == np.array([0, 0, 1]))
-    assert np.all(pnts[1] == np.array([1, 0, 1]))
-    assert np.all(pnts[2] == np.array([0, 1, 1]))
-    assert np.all(pnts[3] == np.array([1, 1, 1]))
-    assert np.all(pnts[4] == np.array([1, 0, 0]))
-    assert np.all(pnts[5] == np.array([0, 0, 0]))
-    assert np.all(pnts[6] == np.array([1, 1, 0]))
-    assert np.all(pnts[7] == np.array([0, 1, 0]))
+    assert len(pnts) == 8 and len(pnts[0]) == 3
+    assert all(i == j for i, j in zip(pnts[0], [0, 0, 1]))
+    assert all(i == j for i, j in zip(pnts[1], [1, 0, 1]))
+    assert all(i == j for i, j in zip(pnts[2], [0, 1, 1]))
+    assert all(i == j for i, j in zip(pnts[3], [1, 1, 1]))
+    assert all(i == j for i, j in zip(pnts[4], [1, 0, 0]))
+    assert all(i == j for i, j in zip(pnts[5], [0, 0, 0]))
+    assert all(i == j for i, j in zip(pnts[6], [1, 1, 0]))
+    assert all(i == j for i, j in zip(pnts[7], [0, 1, 0]))
 
 
 def test_normals():
@@ -44,9 +43,8 @@ def test_normals():
     facets, pnts = stl.toindexed(vertices)
     ni, nv = stl.normals(facets, pnts)
     assert len(ni) == 12  # 6 faces, 2 facets/face.
-    p = np.arange(6)
-    assert np.all(ni == np.vstack((p, p)).T.reshape((1, -1)))
-    assert nv.shape == (6, 3)
+    assert all(i == j for i, j in zip(ni, [p for p in range(6) for k in range(2)]))
+    assert len(nv) == 6 and len(nv[0]) == 3
 
 
 def test_text():
