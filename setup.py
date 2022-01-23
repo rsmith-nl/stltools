9752dc2ab13d3854faa52b67f1bff90133e35f21
Roland Smith <rsmith@xs4all.nl>
2022-01-23 18:07:44 +0100

    Updated setup script with uninstall option.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@
 # Copyright © 2020 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2020-10-25T12:18:04+0100
-# Last modified: 2022-01-18T00:16:29+0100
-"""Script to install self-contained scripts for the local user."""
+# Last modified: 2022-01-23T18:07:08+0100
+"""Install self-contained scripts for the local user."""
 
 import os
 import py_compile
@@ -17,16 +17,46 @@ import tempfile
 import zipfile as z
 
 
-def main():
-    """Entry point for the setup script."""
-    # What to install; (name, module, main, nt-extension)
-    scripts = [
+# What to install; (installed name, module, script, nt-extension)
+SCRIPTS = (
         ("stl2pov", "stltools", "stl2pov.py", ".py"),
         ("stl2ps", "stltools", "stl2ps.py", ".py"),
         ("stl2pdf", "stltools", "stl2pdf.py", ".py"),
         ("stlinfo", "stltools", "stlinfo.py", ".py"),
-    ]
-    # Preparation
+)
+
+
+def main():
+    """Entry point for the setup script."""
+    dirs = dirnames()
+    cmd = None
+    if len(sys.argv) == 2:
+        cmd = sys.argv[1].lower()
+    if cmd == "install":
+        # Create primary installation directory if it doesn't exist.
+        if not os.path.exists(dirs[0]):
+            os.makedirs(dirs[0])
+            print(f"Created “{dirs[0]}”. Do not forget to add it to your $PATH.")
+    elif cmd in ("uninstall", "clean"):
+        pass
+    else:
+        print(f"Usage {sys.argv[0]} [install|uninstall|clean]")
+    # Actual (de)installation.
+    for installed_name, module, script, nt_ext in SCRIPTS:
+        names = destnames(installed_name, nt_ext, dirs)
+        if cmd == "install":
+            do_install(installed_name, module, script, nt_ext, names)
+        elif cmd == "uninstall":
+            do_uninstall(names)
+        elif cmd == "clean":
+            remove(installed_name)
+        else:
+            print(f"* '{script}' would be installed as '{names[0]}'")
+            if names[1]:
+                print(f"  or '{names[1]}'")
+
+
+def dirnames():
     if os.name == "posix":
         destdir = sysconfig.get_path("scripts", "posix_user")
         destdir2 = ""
@@ -36,42 +66,33 @@ def main():
     else:
         print(f"The system '{os.name}' is not recognized. Exiting")
         sys.exit(1)
-    install = "install" in [a.lower() for a in sys.argv]
-    if install:
-        if not os.path.exists(destdir):
-            os.makedirs(destdir)
-    else:
-        print("(Use the 'install' argument to actually install scripts.)")
-    do_install(install, scripts, destdir, destdir2)
+    return destdir, destdir2
 
 
-def do_install(install, scripts, destdir, destdir2):
+def destnames(base, nt_ext, dest):
+    if os.name == "posix":
+        destname = dest[0] + os.sep + base
+        destname2 = ""
+    elif os.name == "nt":
+        destname = dest[0] + os.sep + base + nt_ext
+        destname2 = dest[1] + os.sep + base + nt_ext
+    return destname, destname2
+
+
+def do_install(nm, module, main, nt_ext, names):
     # Actual installation.
-    for nm, module, main, nt_ext in scripts:
-        remove(nm)
-        mkarchive(nm, module, main=main)
-        base = os.path.splitext(nm)[0]
-        if os.name == "posix":
-            destname = destdir + os.sep + base
-            destname2 = ""
-        elif os.name == "nt":
-            destname = destdir + os.sep + base + nt_ext
-            destname2 = destdir2 + os.sep + base + nt_ext
-        if install:
-            for d in (destname, destname2):
-                try:
-                    shutil.copyfile(nm, d)
-                    print(f"* installed '{nm}' as '{destname}'.")
-                    os.chmod(d, 0o700)
-                    break
-                except (OSError, PermissionError, FileNotFoundError):
-                    pass  # Can't write to destination
-            else:
-                print(f"! installation of '{nm}' has failed.")
-        else:
-            print(f"* '{nm}' would be installed as '{destname}'")
-            if destname2:
-                print(f"  or '{destname2}'")
+    remove(nm)
+    mkarchive(nm, module, main=main)
+    for n in names:
+        try:
+            shutil.copyfile(nm, n)
+            print(f"* installed '{nm}' as '{n}'.")
+            os.chmod(n, 0o700)
+            break
+        except (OSError, PermissionError, FileNotFoundError):
+            pass  # Can't write to destination
+    else:
+        print(f"! installation of '{nm}' has failed.")
 
 
 def mkarchive(name, modules, main="__main__.py"):
@@ -113,12 +134,20 @@ def mkarchive(name, modules, main="__main__.py"):
     os.chmod(name, 0o755)
 
 
+def do_uninstall(names):
+    for n in names:
+        if n and remove(n):
+            print(f"The file '{n}' has been removed.")
+
+
 def remove(path):
     """Remove a file, ignoring directories and nonexistant files."""
     try:
         os.remove(path)
+        return True
     except (FileNotFoundError, PermissionError, IsADirectoryError, OSError):
         pass
+    return False
 
 
 if __name__ == "__main__":
